a099fef8a284b9dc4f9a6e2557a6bcf7bc613acd
Dan Uluma <danuluma@gmail.com>
2018-12-06 23:24:25 +0300

    day2 part1

--- day2/day2.rb
+++ day2/day2.rb
@@ -3,14 +3,16 @@ f = File.read("data.txt").split().to_a
 twos = 0
 threes = 0
 f.each do |line|
+  three = false
+  two = false
   l = line.split('').to_a
   l.uniq.each do |xter|
     count = l.count(xter)
-    if count ==3
-      threes += 1
-    elsif count==2
-      twos += 1
-    end
+    three = true if count ==3
+    two = true if count==2
   end
+  twos +=1 if two
+  threes +=1 if three
+
 end
 puts twos*threes
