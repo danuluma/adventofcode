0d52433a595b35aab794b4abb503b9d14dc1eedb
Dan Uluma <danuluma@gmail.com>
2020-12-05 02:08:22 +0300

    day 4

--- 2020/day4/sol2.rb
+++ 2020/day4/sol2.rb
@@ -10,32 +10,26 @@ req_keys = ["byr" ,
 
 
 def validate pdata
-    vals = pdata.strip.map { |i| i.split(":")}
+    vals = pdata.map { |i| i.split(":")}
     vals = vals.to_h
-    return false if vals["byr"].to_i < 1920 || vals["byr"].to_i > 2002
-    return false if vals["iyr"].to_i < 2010 || vals["iyr"].to_i > 2020
-    return false if vals["eyr"].to_i < 2020 || vals["eyr"].to_i > 2030
-    if vals["hgt"].include? "cm"
-        hgt = vals["hgt"].split("cm")[0].to_i
+    return false if (vals["byr"].to_i < 1920 || vals["byr"].to_i > 2002)
+    return false if (vals["iyr"].to_i < 2010 || vals["iyr"].to_i > 2020)
+    return false if (vals["eyr"].to_i < 2020 || vals["eyr"].to_i > 2030)
+    return false if !vals["hgt"].match(/[cm|in]$/)
+    if vals["hgt"].match(/cm$/)
+        hgt = vals["hgt"].to_i
         return false if hgt < 150 || hgt > 193
-    end
-    if vals["hgt"].include? "in"
-        hgt = vals["hgt"].split("in")[0].to_i
+    elsif vals["hgt"].match(/in$/)
+        hgt = vals["hgt"].to_i
         return false if (hgt < 59 || hgt > 76)
     end
 
-    if !vals["hcl"].match(/^#([0-9]|[abcdef]){6,6}$/)
-        return false
-    end
-
-    if !["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include?(vals["ecl"].strip)
-        # puts vals["ecl"]
-        return false
-    end
-    if vals["pid"].size != 9 
-        # puts vals['pid']
-        return false
-    end
+    return false if !vals["hcl"].match(/^#([a-f\d]){6,6}$/)
+        
+    return false if !["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include?(vals["ecl"].strip)
+        
+    return false if !vals["pid"].match(/^\d{9,9}$/) 
+    
     return true
 end
 
