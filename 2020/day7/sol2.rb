fce111f684c6f1bbb99409bbe94830f47e063192
Dan Uluma <danuluma@gmail.com>
2020-12-08 13:36:53 +0300

    Day 7 part 2

---
+++ 2020/day7/sol2.rb
@@ -0,0 +1,49 @@
+require 'set'
+
+
+data = File.readlines("#{__dir__}/data.txt")
+
+
+def colours_hash d
+    d.filter_map do |l|
+        color = l.split[0..1].join(' ')
+        contained = l.split('contain')[1].split(',').filter_map do |i| 
+            count =  i.split.first
+            c = i.split[1..2].join(' ')
+            [c, count] unless c == "other bags."
+        end
+        [color, contained]
+    end
+end
+
+tot = 0
+def find_bags search, hs
+    res = hs.filter_map do |f| 
+        f[1] if f[0] == search
+    end.first
+    return res
+end
+
+def sss sum, init, hd
+    tot = []
+    while init.size > 0 
+        t = init.shift
+        tot << t
+        e,f = t
+        res = find_bags e, hd
+        res.each do  |e,g| 
+           init << [e, g.to_i*f]
+        end
+    end
+    return tot.map { |m| m[1]}.sum
+end
+
+def find_shiny_gold dt
+    hash = colours_hash dt
+    res = sss 0, [['shiny gold', 1]], hash 
+    # Exclude the outer shiny gold bag from count
+    return res - 1
+end
+
+puts find_shiny_gold data
+
